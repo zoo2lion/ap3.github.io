5b4fc181befb9bf0e047662e3e99c21ba5ab3cb0
zoo2lion <83560338+zoo2lion@users.noreply.github.com>
2021-05-03 21:01:15 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ap3.github.io
+# Easy-Web-TV-M3u8
+ Easy way to watch iptv on a web and support m3u8 links
+
+# Official Websites
+ [Demo](https://zhangboheng.github.io/Easy-Web-TV-M3u8/)
---
+++ js/main.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+
+    $("#video1").width($("#div1").width()).height($("#div1").height())
+
+    var player = videojs(document.querySelector('#video1'));
+
+    //Get Current href
+    var url = window.location.href;
+    var key = url.split("/").slice(-1)[0].split(".")[0]
+        //Get iptv-org m3u list and show contents lists
+    $.ajax({
+        type: "GET",
+        url: ' https://iptv-org.github.io/iptv/categories/' + key + ".m3u",
+        success: function(message, text, response) {
+            $("ul").empty()
+            let str = message
+            let lst = str.split(",").slice(1, ).filter(x => /[^h]+.m3u8/.test(x)).map(x => x.split("\n"))
+            let array = str.split(" ")
+            console.log(str)
+            let links = array.filter(x => /[^h]+.m3u8/.test(x)).map(x => x.split("\n")).flat().filter(x => /[^h]+.m3u8/.test(x))
+            for (let i = 0; i < links.length; i++) {
+                $("ul").append("<li>" + "<p title=" + links[i] + ">" + lst[i][0] + "</p>" + "</li>")
+            }
+
+            $("li p").click(function() {
+                player.src({
+                    src: $(this).attr("title"),
+                    type: 'application/x-mpegURL' /*video type*/
+                });
+
+                player.play();
+            })
+
+            let menuHeight = document.getElementById('menu');
+            let screenHeight = window.innerHeight;
+            menuHeight.style.height = screenHeight - 60 + "px";
+            $("#menu").css({ "overflow-y": "auto", "height": menuHeight });
+        },
+        fail: function(xhr, textStatus, errorThrown) {
+            alert("Please check your Internet or the iptv source has gone out!")
+        }
+    })
+
+    $("#player").on({
+        mouseenter: function() {
+            $(this).css({ "opacity": 1 })
+        },
+        click: function() {
+            $(this).css({ "background-image": "url(../images/player.jpg)", "border": "1px solid #fff" })
+            if (window.width > 640) {
+                $("input").show()
+            } else {
+                $("input").toggle()
+            }
+            let link = $("input").val()
+            if (link.length > 0) {
+                player.src({
+                    src: link,
+                    type: 'application/x-mpegURL' /*video type*/
+                });
+                player.play();
+            }
+            $("input").val("")
+        },
+        mouseleave: function() {
+            $(this).css({ "opacity": 0.5 })
+        }
+    });
+    $("#prev").on({
+        mouseenter: function() {
+            $(this).css({ "opacity": 1 })
+        },
+        click: function() {
+            window.location.href = "/Easy-Web-TV-M3u8/";
+        },
+        mouseleave: function() {
+            $(this).css({ "opacity": 0.5 })
+        }
+    });
+    $("#github").on({
+        mouseenter: function() {
+            $(this).css({ "opacity": 1 })
+        },
+        click: function() {
+            window.open("https://github.com/zhangboheng/Easy-Web-TV-M3u8");
+        },
+        mouseleave: function() {
+            $(this).css({ "opacity": 0.5 })
+        }
+    });
+    $("input").on({
+        mouseenter: function() {
+            $(this).css({ "opacity": 1 })
+        },
+        mouseleave: function() {
+            $(this).css({ "opacity": 0.5 })
+            $(this).hide(2000)
+            $("#player").css({ "background-image": "url(../images/link.jpg)" })
+        }
+    });
+})
